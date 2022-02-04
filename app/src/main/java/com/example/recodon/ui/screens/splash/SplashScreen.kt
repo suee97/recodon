5d1143e4c6d5c00e87aefb7d7354f7a76d0f9f2b
yubin <dbqls2821@gmail.com>
2022-02-04 12:45:49 +0900

    feat: Splash Screen

--- app/src/main/java/com/example/recodon/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/recodon/ui/screens/splash/SplashScreen.kt
@@ -1,11 +1,32 @@
 package com.example.recodon.ui.screens.splash
 
+import android.view.Surface
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.delay
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
+import com.example.recodon.R
+
 
 // 스플래쉬 화면
 @Composable
@@ -13,12 +34,81 @@ fun SplashScreen(
     navController: NavHostController
 ) {
     LaunchedEffect(key1 = true) {
-        delay(200) // 테스트 때문에 줄여놓음. 원래 2~3초
+        delay(2000) // 테스트 때문에 줄여놓음. 원래 2~3초
         navController.navigate("home")
     }
 
-    Text(
-        text = "스플래쉬 화면",
-        fontSize = 30.sp
+    Loader()
+
+}
+
+@Composable
+private fun Loader() {
+    val fontFamily = FontFamily(
+        Font(R.font.lexend_light, FontWeight.Light),
+        Font(R.font.lexend_regular, FontWeight.Normal),
+        Font(R.font.lexend_medium, FontWeight.Medium),
+        Font(R.font.lexend_semibold, FontWeight.SemiBold),
+        Font(R.font.lexend_bold, FontWeight.Bold),
+        Font(R.font.lexend_extrabold, FontWeight.ExtraBold)
     )
+    
+    val compositionResult : LottieCompositionResult =
+        rememberLottieComposition(
+            spec = LottieCompositionSpec.RawRes(
+                R.raw.splashimage
+            )
+        )
+
+    val progress by animateLottieCompositionAsState(
+        compositionResult.value,
+        isPlaying = true,
+        iterations = LottieConstants.IterateForever,
+        speed = 1.0f
+    )
+
+    // Create a MutableTransitionState<Boolean> for the AnimatedVisibility.
+    val state = remember {
+        MutableTransitionState(false).apply {
+            // Start the animation immediately.
+            targetState = true
+        }
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+
+
+            LottieAnimation(
+                compositionResult.value,
+                progress,
+                modifier = Modifier.size(250.dp)
+            )
+
+
+            Text(
+                "Envelope\nFor Our Planet",
+                color = Color(0xFF479156),
+                fontSize = 30.sp,
+                fontFamily = fontFamily,
+                modifier = Modifier
+                    .padding(2.dp),
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.Center
+
+                )
+
+
+        }
+    }
 }
